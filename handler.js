e9703febfcf4bfb982fd66d0d09e470cd59287a1
Hannah <hannahbeardmore1@hotmail.co.uk>
2020-04-27 18:39:01 +0100

    GET, DELETE, POST & PUT endpoints now talk to my databasegit status All checked and working on Postman

--- handler.js
+++ handler.js
@@ -1,87 +1,92 @@
+/* eslint-disable max-len */
 const serverlessHttp = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
+const mysql = require("mysql");
 
-// Logically seperate 4 sections of code accorind to the method of the HTTPS request received
-
-// Export a singe function, called app
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'Tasks'
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/tasks', function(request, response) {
-
   // request had loads of info about the request
   // response has some useful methos for sending a response
-
-  response.status(200).send({
-    tasks: [
-      {
-        id: 1,
-        text: 'Research photographers',
-        completed: true,
-        date: new Date(2020, 04, 02),
-      },
-      {
-        id: 2,
-        text: 'Decide on theme',
-        completed: false,
-        date: new Date(2020, 04, 03),
-      },
-      {
-        id: 3,
-        text: 'Get engagement ring resized',
-        completed: false,
-        date: new Date(2020, 04, 13),
-      },
-    ],
+  connection.query('SELECT * FROM TaskList', function(err, data) {
+    if (err) {
+      console.log('Error from MYSQL', err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(data);
+    }
   });
 });
 
 app.delete('/tasks/:id', function(request, response) {
   const taskIdToBeDeleted = request.params.id;
-
-  let someResponse = {
-    message: 'Task ' + taskIdToBeDeleted + ' Deleted!'
-  };
-
-  if (taskIdToBeDeleted > 3) {
-    response.status(404);
-    someResponse = {
-      message: 'Task ' + taskIdToBeDeleted + ' does not exist'
-    };
-  }
-
-  response.json(someResponse);
-
-  // should delete the task with the specified ID from the database
+  connection.query(`DELETE FROM TaskList WHERE TaskId = ${taskIdToBeDeleted}`, function(err) {
+    if (err) {
+      console.log('Error from MySQL', err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(`Task ${taskIdToBeDeleted} Deleted!`);
+    }
+  });
 });
 
 app.post('/tasks', function(request, response) {
-
-  response.status(201).json({
-    message: 'Task '+ request.body.text + ' Created!'
+  const inputData = request.body;
+  const query = `INSERT INTO TaskList (Description, Urgent, Completed, Date) VALUES(?, ?, ?, ?)`;
+  connection.query(query, [inputData.Description, inputData.Urgent, false, inputData.Date], function(err, results) {
+    if (err) {
+      console.log('Error from my SQL', err);
+      response.status(500).send(err);
+    } else {
+      connection.query(`SELECT * FROM TaskList WHERE TaskId = ${results.insertId}`, function(err, results) {
+        if (err) {
+          console.log('Error from MySQL', err);
+          response.status(500).send(err);
+        } else {
+          response.status(201).send(results[0]);
+        }
+      });
+    }
   });
-  // should insert a new task in the database
 });
 
 app.put('/tasks/:id', function(request, response) {
+  const inputData = request.body;
   const taskIdToBeUpdated = request.params.id;
+  connection.query(`UPDATE TaskList SET Description = '${inputData.Description}', Urgent = ${inputData.Urgent},  Completed = ${inputData.Completed}  WHERE TaskId = ${19}`, function(err) {
+    if (err) {
+      console.log('Error from MySQL', err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(`Task ${taskIdToBeUpdated} Updated!`);
+    }
+  });
 
-  let someResponse = {
-    message: 'Task ' + taskIdToBeUpdated + ' has recieved a request to be updated'
-  };
+  // const taskIdToBeUpdated = request.params.id;
 
-  if (taskIdToBeUpdated > 3) {
-    response.status(404);
-    someResponse = {
-      message: 'Task ' + taskIdToBeUpdated + ' does not exist'
-    };
-  }
+  // let someResponse = {
+  //   message: 'Task ' + taskIdToBeUpdated + ' has recieved a request to be updated'
+  // };
 
-  response.json(someResponse);
-  
+  // if (taskIdToBeUpdated > 3) {
+  //   response.status(404);
+  //   someResponse = {
+  //     message: 'Task ' + taskIdToBeUpdated + ' does not exist'
+  //   };
+  // }
+
+  // response.json(someResponse);
+
   // response.status(200).send('Task Updated!')
   // should update a task into the database
 });
